6796b0d326f1ccd9b61dc7b966aa16e89d24831a
pierreguillot <guillotpierre6@gmail.com>
2015-08-21 19:12:27 +0200

    gobj->text = NULL to avoid crashes

--- Sources/egraphics.c
+++ Sources/egraphics.c
@@ -79,6 +79,7 @@ static void egraphics_paint(t_elayer *g, int filled, int preserved)
             nobj->e_type      = g->e_new_objects.e_type;
             nobj->e_npoints   = g->e_new_objects.e_npoints;
             nobj->e_points = (t_pt*)malloc((size_t)nobj->e_npoints * sizeof(t_pt));
+            nobj->e_text = NULL;
             if(!nobj->e_points)
             {
                 nobj->e_npoints = 0;
