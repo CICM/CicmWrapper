7d9f5eb816d0dbb96a0ca9e0b82335f7d8d57287
Antoine Villeret <antoine.villeret@gmail.com>
2019-04-24 16:06:29 +0200

    fix Visual Studio build

--- Sources/ebox.c
+++ Sources/ebox.c
@@ -945,8 +945,7 @@ t_pd_err ebox_set_font(t_ebox *x, t_object *attr, int argc, t_atom *argv)
     else
         x->b_font.c_family = gensym("Helvetica");
 
-    x->b_font.c_family = gensym(strtok(x->b_font.c_family->s_name," ',.-"));
-    x->b_font.c_family->s_name[0] = (char)toupper(x->b_font.c_family->s_name[0]);
+    x->b_font.c_family = gensym(toupper(strtok(x->b_font.c_family->s_name," ',.-")));
     return 0;
 }
 
